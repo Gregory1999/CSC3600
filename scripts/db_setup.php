b6c32728b94cba2a471f6bdf9ca3510036ef4856
Greg <U1023488@umail.usq.edu.au>
2018-08-14 16:32:21 +0930

    added a new table to hold file path

--- scripts/db_setup.php
+++ scripts/db_setup.php
@@ -8,9 +8,15 @@
 	#this table will store the root directory information-  may later be able to extend code to have multiple selectable roots
 	$db->exec('CREATE TABLE root_directory
 				(path STRING PRIMARY Key,
-				 last_scan INTEGER)'); # may change this data type
+				 last_scan DATETIME)'); # may change this data type
 				
-				
+	//this will grow- just added the minimum to allow testing			
+	$db->exec('CREATE TABLE photo
+				(photo_path STRING PRIMARY Key,
+				 date_created DATETIME,
+				 date_modified DATETIME)');
+				 
+
 				
 
 ?>
