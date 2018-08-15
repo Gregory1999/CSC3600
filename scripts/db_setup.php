9e5c647ee51141ca802c95758ad56e92f801deb4
Greg <U1023488@umail.usq.edu.au>
2018-08-15 18:27:51 +0930

    general dev

--- scripts/db_setup.php
+++ scripts/db_setup.php
@@ -1,20 +1,29 @@
 <?php
-# this file is to set up the initial db when the program is initially run
-	echo "db_setup file";	
-	
-	
-	$db = new SQLite3('site.db');
-	
-	#this table will store the root directory information-  may later be able to extend code to have multiple selectable roots
-	$db->exec('CREATE TABLE root_directory
+//this file is to set up the db 
+	//if no database file- create the db
+	if (!file_exists('site.db')){
+		$db = new SQLite3('site.db');
+						
+		#this table will store the root directory information-  may later be able to extend code to have multiple selectable roots
+		$db->exec('CREATE TABLE root_directory
 				(path STRING PRIMARY Key,
 				 last_scan DATETIME)'); # may change this data type
 				
-	//this will grow- just added the minimum to allow testing			
-	$db->exec('CREATE TABLE photo
+		//this will grow- just added the minimum to allow testing			
+		$db->exec('CREATE TABLE photo
 				(photo_path STRING PRIMARY Key,
 				 date_created DATETIME,
 				 date_modified DATETIME)');
+
+
+	}
+	//if file exists point to database
+	else {
+		$db = new SQLite3('site.db');
+	}
+	
+	
+	
 				 
 
 				
