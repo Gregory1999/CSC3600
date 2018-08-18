f08782d984d998749c9710130e3acb4d8d197b68
Greg <U1023488@umail.usq.edu.au>
2018-08-18 16:02:07 +0930

    changed to use JSON to input the images

--- get_images.php
+++ get_images.php
@@ -1,6 +1,12 @@
 <?php
 //this file will send all images on request
 
+			//this is just here to test the load screen
+			//sleep(5);
+			
+			
+			$json = "{";
+			
 			include_once "scripts/db_setup.php";
 
 			//check for for directory path, if one does not exist then ask for user to select
@@ -16,13 +22,20 @@
 			
 				//initially sets the last scan time to force initial scan
 				$date = new DateTime('1970-01-01');
+				
+				//retrieve the directory
+				
+				
 							
 				//hard code root directory for testing- to be deleted
 				$query="INSERT INTO root_directory(path, last_scan) VALUES ( './Test_Images', '" . $date->format('d-m-Y H:i:s') . "' ) ";
-				$db->query($query);				
+				$db->query($query);	
+				
+				$json .= '"root":"NULL"';		
 			
 			// 
-			print "no_directory";
+			//print "no_directory";
+			
 
 			
 			}
@@ -33,21 +46,28 @@
 				// scan file system script
 				include_once "scripts/file_scanner.php";
 			
-			
-				//display images
+				$json .= '"root":" ' . $root_path . '" ,"' . imageArray . '" : [';
+								//display images
 				$query = "SELECT photo_path, date_created FROM photo ORDER BY date_created DESC";
 				$result= $db->query($query);
 				
 				while( $row = $result->fetchArray(SQLITE3_ASSOC)) {
 					$photo_path= $row["photo_path"];
-					print "<img src='$photo_path' class='img-thumbnail' >";
+					$json .= ' "' . $photo_path . '",';
+					
+					
+					//print "<img src='$photo_path' class='img-thumbnail' >";
 				
 				}
-				
+				$json = rtrim($json,",") . "]";
 				
 				
 			}
-
+			$json .=  "}";
+			//add json header and send the data
+    		header("Content_type: text/json");
+    		print $json;
+				
 
 
 
