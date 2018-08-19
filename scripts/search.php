ef06aa33444ee885af86907955383b5253b5ea9e
Greg <U1023488@umail.usq.edu.au>
2018-08-19 14:59:52 +0930

    added a very basic search

--- scripts/search.php
+++ scripts/search.php
@@ -4,8 +4,35 @@
 //reply will be formatted in JSON
 //search criteria to be passed to script using Get
 	$json = "{";
+	
+	// scan file system 
+	include_once "scripts/file_scanner.php";
+	$db = new SQLite3('../site.db');
+	
 	$simple_search= $_GET['simple'];
-	$json .=  '"imageArray" : ["Test_Images/ute.jpeg"]';
+	
+	//search db for simple search string
+	//add all matches to JSON imageArray
+	
+	$json .= '"imageArray" : [';
+	
+	$query = "SELECT photo_path, date_created FROM photo WHERE photo_path LIKE '%${simple_search}%' ORDER BY date_created DESC";
+	//$query = "SELECT photo_path, date_created FROM photo ORDER BY date_created DESC";
+	
+	$result= $db->query($query);
+	
+	while( $row = $result->fetchArray(SQLITE3_ASSOC)) {
+					$photo_path= $row["photo_path"];
+					$json .= ' "' . $photo_path . '",';
+					
+					
+					//print "<img src='$photo_path' class='img-thumbnail' >";
+				
+				}
+				$json = rtrim($json,",") . "]";
+	
+	
+	
 
 	$json .=  "}";
 	//add json header and send the data
