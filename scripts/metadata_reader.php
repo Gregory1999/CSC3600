2eaa2ef1029106b1249ebf670937da84d04da692
Greg <42056523+Gregory1999@users.noreply.github.com>
2018-09-06 16:15:18 +0930

    refactored code

--- scripts/metadata_reader.php
+++ scripts/metadata_reader.php
@@ -4,55 +4,23 @@
 
 	ini_set('exif.encode_unicode', 'UTF-8');
 	$exif = exif_read_data($image, 'ANY_TAG', true);	
-
-  
-	// get the time that the photo was taken
-	if (!empty($exif['EXIF']['DateTimeOriginal'])) {
-		//$exif_date = $exif_exif['DateTimeOriginal'];
-		$exif_date = $exif['EXIF']['DateTimeOriginal'];
-	}
-	else{
-		//no photo date data 	
-		$exif_date= '';		
-	}
 	
-	// get the title tag
-	if (!empty($exif['IFD0']['Title'])) {
+	//arrays of all stored metadata
+	$ifd0_data = array("Title"=>"", "Comments"=>"","Keywords"=>"", "Subject"=>"");
+	$exif_data =array("DateTimeOriginal"=>"");
+	
+	//retrieve exif section data
+	foreach ( $exif_data as $key => $value ){
+		if (!empty($exif["EXIF"]["$key"])) {
+			$exif_data["$key"] = $exif["EXIF"]["$key"];
+		}
+	}
+	//retrieve ifd0 data
+	foreach ( $ifd0_data as $key => $value ){
+		if (!empty($exif["IFD0"]["$key"])) {
 			//windows put in a heap of randome chars
-			$exif_title= preg_replace('/[[:^print:]]/', '', $exif['IFD0']['Title']);	
-	}
-	else{
-		//no photo date data 	
-		$exif_title= '';		
-	}
-	
-	// get the Comments tag
-	if (!empty($exif['IFD0']['Comments'])) {
-			//windows put in a heap of randome chars, so  remove
-			$exif_comments= preg_replace('/[[:^print:]]/', '', $exif['IFD0']['Comments']);		
-	}
-	else{
-		//no photo date data 	
-		$exif_comments= '';		
-	}
-	//get the tags/Keywords
-	if (!empty($exif['IFD0']['Keywords'])) {
-			//windows put in a heap of randome chars, so remove
-			$exif_keywords= preg_replace('/[[:^print:]]/', '', $exif['IFD0']['Keywords']);				
-	}
-	else{
-		//no photo date data 	
-		$exif_keywords = '';		
-	}
-	
-	// get the subject tag
-	if (!empty($exif['IFD0']['Subject'])) {
-			//windows put in a heap of randome chars, so  remove
-			$exif_subject= preg_replace('/[[:^print:]]/', '', $exif['IFD0']['Subject']);		
-	}
-	else{
-		//no photo date data 	
-		$exif_subject= '';		
+			$ifd0_data["$key"]= preg_replace("/[[:^print:]]/", "", $exif["IFD0"]["$key"]);	
+		}
 	}
 	
 	$image_thumbnail = exif_thumbnail($image);
@@ -118,12 +86,12 @@
 	}
 	
 	//insert or replace the db
-	$query="INSERT OR REPLACE INTO photo_file(photo_path, date_created) VALUES ( '$imagePath', '$exif_date') ";
+	$query="INSERT OR REPLACE INTO photo_file(photo_path, date_created) VALUES ( '$imagePath', '" . $exif_data['DateTimeOriginal'] . "') ";
 	$db->query($query);
-	$query="INSERT  OR REPLACE INTO photo_description(photo_path, title, comments, tags, subject) VALUES ( '$imagePath', '$exif_title','$exif_comments', '$exif_keywords', '$exif_subject') ";
+	$query="INSERT  OR REPLACE INTO photo_description(photo_path, title, comments, tags, subject) VALUES ( '$imagePath', '" . $ifd0_data['Title'] . "','" . $ifd0_data['Comments'] . "', '" . $ifd0_data['Keywords'] . "', '" . $ifd0_data['Subject'] . "') ";
 	$db->query($query);
 	
-	//insert the thumbnail image
+	//insert the thumnail image
 	$query="INSERT OR REPLACE INTO photo_thumbnail(photo_path, photo_thumbnail) VALUES ( '$imagePath', :image_thumbnail) ";
 	$stmt= $db->prepare($query);
 	$stmt->bindParam(':image_thumbnail', $image_thumbnail, \PDO::PARAM_LOB);
