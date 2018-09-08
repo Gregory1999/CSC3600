58ba2c6c097802c604374cf1935fdf9641887802
Ryan <u1091311@umail.usq.edu.au>
2018-09-08 13:28:05 +1000

    fixed error with pel classes
    added use classes to metadata_reader.php to fix errors on ryan's xampp

--- scripts/metadata_reader.php
+++ scripts/metadata_reader.php
@@ -3,22 +3,29 @@
 //it is used to group all the metadata extraction code
 
 	ini_set('exif.encode_unicode', 'UTF-8');
-	$exif = exif_read_data($image, 'ANY_TAG', true);	
-	
+	$exif = exif_read_data($image, 'ANY_TAG', true);
+
 	//could probably process all sections using a loop<----------------
-	
+
 	//arrays of all stored metadata
 	$ifd0_data = array("Title"=>"", "Comments"=>"","Keywords"=>"", "Subject"=>"", "Author"=>"", "Copyright"=>"");
 	$exif_data = array("DateTimeOriginal"=>"", "ExifImageLength"=>"", "ExifImageWidth"=>"", "CompressedBitsPerPixel"=>"");
 	$file_data = array("FileName"=>"", "FileDateTime"=>"","FileSize"=>"", "MimeType"=>"");
-	
+
 	//retrieve file  data
 	foreach ( $file_data as $key => $value ){
 		if (!empty($exif["FILE"]["$key"])) {
 			$file_data["$key"] = $exif["FILE"]["$key"];
 		}
 	}
-	
+
+	//Pel classes
+	use lsolesen\pel\PelDataWindow;
+	use lsolesen\pel\PelJpeg;
+	use lsolesen\pel\PelTag;
+	use lsolesen\pel\PelIfd;
+	use lsolesen\pel\PelEntryAscii;
+
 	//retrieve exif section data
 	foreach ( $exif_data as $key => $value ){
 		if (!empty($exif["EXIF"]["$key"])) {
@@ -29,12 +36,12 @@
 	foreach ( $ifd0_data as $key => $value ){
 		if (!empty($exif["IFD0"]["$key"])) {
 			//windows put in a heap of randome chars
-			$ifd0_data["$key"]= preg_replace("/[[:^print:]]/", "", $exif["IFD0"]["$key"]);	
+			$ifd0_data["$key"]= preg_replace("/[[:^print:]]/", "", $exif["IFD0"]["$key"]);
 		}
 	}
-	
+
 	$image_thumbnail = exif_thumbnail($image);
-	
+
 	//creates a thumbnail image if one does not exist
 	if ($image_thumbnail === False) {
 		//This code was referenced from exif_thumbnail php man page
@@ -44,18 +51,18 @@
 		$jpeg = new PelJpeg($image);
 		$exif = $jpeg->getExif();
 		$tiff = $exif->getTiff();
-		$ifd0 = $tiff->getIfd();       
+		$ifd0 = $tiff->getIfd();
 
 		$ifd1 = $ifd0->getNextIfd();
 		//if ifd1 doesnt exist then create it
 		if (!$ifd1) {
 			//create new ifd1 and point ifd0 to it
 			$ifd1 = new PelIfd(1);
-			$ifd0->setNextIfd($ifd1); 
-			$origImage = ImageCreateFromString($jpeg->getBytes()); 
+			$ifd0->setNextIfd($ifd1);
+			$origImage = ImageCreateFromString($jpeg->getBytes());
 			$width=imagesx($origImage);
 			$height=imagesy($origImage);
-			
+
 			$widthMax = 150;
 			$heightMax = 100;
 
@@ -63,39 +70,39 @@
 				$thumb_w=$widthMax;
 				$thumb_h=$heightMax;
 				if ($thumb_w/$width*$height>$thumb_h)
-					$thumb_w=round($thumb_h*$width/$height); 
+					$thumb_w=round($thumb_h*$width/$height);
 				else
 					$thumb_h=round($thumb_w*$height/$width);
 			}
-			else { 
+			else {
 				$thumb_w=$width;
 				$thumb_h=$height;
 			}
 
-				
+
 			$thumb=imagecreatetruecolor($thumb_w,$thumb_h);
 			imagecopyresampled($thumb,$origImage,
 									   0,0,0,0,$thumb_w,$thumb_h,$width,$height);
 
 
-			ob_start();       
-			ImageJpeg($thumb);   
-				
+			ob_start();
+			ImageJpeg($thumb);
+
 			$window = new PelDataWindow(ob_get_clean());
 
-			if ($window) {   
+			if ($window) {
 
-				$ifd1->setThumbnail($window); 
-				$outpath = $image; 
-				file_put_contents($outpath, $jpeg->getBytes()); 
+				$ifd1->setThumbnail($window);
+				$outpath = $image;
+				file_put_contents($outpath, $jpeg->getBytes());
 			}
 		}
-		
-		$image_thumbnail = exif_thumbnail($image);	
-		
+
+		$image_thumbnail = exif_thumbnail($image);
+
 	}
-	
-	//insert or replace the db 
+
+	//insert or replace the db
 	//photo_file table                       <----- might need to move date created
 	$query="INSERT OR REPLACE INTO photo_file(photo_path, date_created, photo_name, photo_type, date_modified, size) VALUES ( '$imagePath', '" . $exif_data['DateTimeOriginal'] . "', '" . $file_data['FileName'] . "', '" . $file_data['MimeType'] . "', '" . date ("d-m-Y h:m:s", $file_data['FileDateTime']) . "', '" . $file_data['FileSize'] . "' ) ";
 	$db->query($query);
@@ -108,15 +115,15 @@
 	//photo image
 	$query="INSERT  OR REPLACE INTO photo_image(photo_path, width, height, compression) VALUES ( '$imagePath', '" . $exif_data['ExifImageWidth'] . "','" . $exif_data['ExifImageLength'] . "', '" . $exif_data['CompressedBitsPerPixel'] . "') ";
 	$db->query($query);
-	
+
 	//photo_camera
 	//photo_advanced
-	
+
 	//insert the thumnail image
 	$query="INSERT OR REPLACE INTO photo_thumbnail(photo_path, photo_thumbnail) VALUES ( '$imagePath', :image_thumbnail) ";
 	$stmt= $db->prepare($query);
 	$stmt->bindParam(':image_thumbnail', $image_thumbnail, \PDO::PARAM_LOB);
 	$stmt->execute();
-	
-	
+
+
 ?>
