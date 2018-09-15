f6478ad3dbbfb5ef22207ff45b7eda03af1305fb
Greg <42056523+Gregory1999@users.noreply.github.com>
2018-09-15 16:38:54 +0930

    created a test file that allows user to select a library folder

---
+++ scripts/browse.php
@@ -0,0 +1,37 @@
+<?php
+//retrieves the path of the folder to display
+//returns paths to all directories to that folder
+
+	//if no path supplied use the document root.
+	if ($_GET['directory'] == ""){
+		//retrieve root from document root.
+		//$directory=$_SERVER["DOCUMENT_ROOT"];
+		
+		//improve this later
+		$directory = "C:\\";
+		$parentDir = "C:\\";
+	}
+	else{
+		$directory = $_GET['directory'];
+		$parentDir = dirname($directory);
+	}
+	chdir("$directory"); 
+	
+
+	//find all directories in this folder.
+	$directoryArray = glob( '*', GLOB_ONLYDIR|GLOB_NOSORT);
+	
+	$json = '{ "parentDirectory" : '. json_encode($parentDir) .', "currentDirectory" : '. json_encode($directory) .', "directoryArray" : [';
+	
+	//output each subdirectory
+	foreach($directoryArray as $subDir){
+		$json .= json_encode(realpath($subDir)) . ', ';
+	
+	}
+	$json = rtrim($json,", ") . "]}";
+	
+	//add json header and send the data
+ 	header("Content_type: text/json");
+ 	print $json;
+
+?>
